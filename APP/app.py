85b76f4364e439fefb8b82320f65b71956764be0
Chaudhary Sarimurrab <samurrab@gmail.com>
2021-06-05 17:15:39 +0530

    Chat Section Added

--- APP/app.py
+++ APP/app.py
@@ -209,7 +209,7 @@ def chat():
         flash('Please login', 'danger')
         return redirect(url_for('login'))
 
-    return render_template("chat.html", username=current_user.name, rooms=ROOMS)
+    return render_template("chat.html", username=current_user.username, rooms=ROOMS)
 
 @socketio.on('incoming-msg')
 def on_message(data):
